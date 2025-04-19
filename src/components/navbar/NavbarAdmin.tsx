53621d27c27fea0e36d1ef8194a47dae9123eeee
Bit9-A <adrian24vergel@gmail.com>
2025-04-18 21:32:36 -0400

    Footer y navbar fija

--- src/components/navbar/NavbarAdmin.tsx
+++ src/components/navbar/NavbarAdmin.tsx
@@ -60,92 +60,92 @@ export default function AdminNavbar(props: {
   const navbarMarginLeft = toggleSidebar ? '0px' : '300px'; // Ajustar margen izquierdo
 
 
-	return (
-		<Box
-		  position="fixed"
-		  boxShadow="none"
-		  bg={useColorModeValue('#310493', '#310493')}
-		  borderColor="transparent"
-		  filter="none"
-		  backdropFilter="blur(20px)"
-		  backgroundPosition="center"
-		  backgroundSize="cover"
-		  borderRadius="16px"
-		  borderWidth="1.5px"
-		  borderStyle="solid"
-		  transition="all 0.3s ease"
-		  alignItems={{ xl: 'center' }}
-		  display={secondary ? 'block' : 'flex'}
-		  minH="75px"
-		  justifyContent={{ xl: 'center' }}
-		  lineHeight="25.6px"
-		  mx="auto"
-		  mt="0px"
-		  pb="8px"
-		  right={{ base: '12px', md: '30px', lg: '30px', xl: '30px' }}
-		  px={{
-			sm: '15px',
-			md: '10px',
-		  }}
-		  ps={{
-			xl: '12px',
-		  }}
-		  pt="8px"
-		  top={{ base: '12px', md: '16px', xl: '18px' }}
-		  w={navbarWidth} // Ancho dinámico
-		  ml={navbarMarginLeft} // Margen dinámico
-		>
-		  <Flex
-			w="100%"
-			flexDirection={{
-			  sm: 'column',
-			  md: 'row',
-			}}
-			alignItems={{ xl: 'center' }}
-			mb="0px"
-		  >
-			<Box mb={{ sm: '8px', md: '0px' }}>
-			  <Breadcrumb>
-				<BreadcrumbItem color="white" fontSize="sm" mb="5px">
-				  <BreadcrumbLink href="#" color="white">
-					Pages
-				  </BreadcrumbLink>
-				</BreadcrumbItem>
-	
-				<BreadcrumbItem color="white" fontSize="sm">
-				  <BreadcrumbLink href="#" color="white">
-					{brandText}
-				  </BreadcrumbLink>
-				</BreadcrumbItem>
-			  </Breadcrumb>
-			  <Link
-				color="white"
-				href="#"
-				bg="inherit"
-				borderRadius="inherit"
-				fontWeight="bold"
-				fontSize="34px"
-				_hover={{ color: 'white' }}
-				_active={{
-				  bg: 'inherit',
-				  transform: 'none',
-				  borderColor: 'transparent',
-				}}
-				_focus={{
-				  boxShadow: 'none',
-				}}
-			  >
+  return (
+	<Box
+	  position="absolute"
+	  boxShadow="none"
+	  bg={useColorModeValue('#310493', '#310493')}
+	  borderColor="transparent"
+	  filter="none"
+	  backdropFilter="blur(20px)"
+	  backgroundPosition="center"
+	  backgroundSize="cover"
+	  borderRadius="16px"
+	  borderWidth="1.5px"
+	  borderStyle="solid"
+	  transition="all 0.3s ease"
+	  alignItems={{ xl: 'center' }}
+	  display={secondary ? 'block' : 'flex'}
+	  minH="75px"
+	  justifyContent={{ xl: 'center' }}
+	  lineHeight="25.6px"
+	  mx="auto"
+	  mt="0px"
+	  pb="8px"
+	  right={{ base: '12px', md: '30px', lg: '30px', xl: '30px' }}
+	  px={{
+		sm: '15px',
+		md: '10px',
+	  }}
+	  ps={{
+		xl: '12px',
+	  }}
+	  pt="8px"
+	  top={{ base: '12px', md: '16px', xl: '18px' }}
+	  w={navbarWidth} // Ancho dinámico
+	  ml={navbarMarginLeft} // Margen dinámico
+	>
+	  <Flex
+		w="100%"
+		flexDirection={{
+		  sm: 'column',
+		  md: 'row',
+		}}
+		alignItems={{ xl: 'center' }}
+		mb="0px"
+	  >
+		<Box mb={{ sm: '8px', md: '0px' }}>
+		  <Breadcrumb>
+			<BreadcrumbItem color="white" fontSize="sm" mb="5px">
+			  <BreadcrumbLink href="#" color="white">
+				Pages
+			  </BreadcrumbLink>
+			</BreadcrumbItem>
+  
+			<BreadcrumbItem color="white" fontSize="sm">
+			  <BreadcrumbLink href="#" color="white">
 				{brandText}
-			  </Link>
-			</Box>
-			<Box ms="auto" w={{ sm: '100%', md: 'unset' }}>
-			  <AdminNavbarLinks
-				onOpen={props.onOpen}
-				secondary={props.secondary}
-				fixed={props.fixed}
-			  />
-			</Box>
-		  </Flex>
+			  </BreadcrumbLink>
+			</BreadcrumbItem>
+		  </Breadcrumb>
+		  <Link
+			color="white"
+			href="#"
+			bg="inherit"
+			borderRadius="inherit"
+			fontWeight="bold"
+			fontSize="34px"
+			_hover={{ color: 'white' }}
+			_active={{
+			  bg: 'inherit',
+			  transform: 'none',
+			  borderColor: 'transparent',
+			}}
+			_focus={{
+			  boxShadow: 'none',
+			}}
+		  >
+			{brandText}
+		  </Link>
 		</Box>
-	  );
+		<Box ms="auto" w={{ sm: '100%', md: 'unset' }}>
+		  <AdminNavbarLinks
+			onOpen={props.onOpen}
+			secondary={props.secondary}
+			fixed={props.fixed}
+		  />
+		</Box>
+	  </Flex>
+	</Box>
+  );
 }
