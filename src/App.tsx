6dfa11558178fad0eaf021666a440a75d0250ad4
Bit9-A <adrian24vergel@gmail.com>
2025-05-03 20:57:50 -0400

    refactor: remove unused components and variables from admin RTL views
    - Deleted tableDataTopCreators.ts as it was no longer needed.
    - Removed CheckTable, ComplexTable, DailyTraffic, PieCard, Tasks, TotalSpent, UserActivity, and WeeklyRevenue components due to redundancy.
    - Cleared out associated variables tableDataCheck and tableDataComplex.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './assets/css/App.css';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import AuthLayout from './layouts/auth';
 import AdminLayout from './layouts/admin';
-import RTLLayout from './layouts/rtl';
+
 
 
 import { ChakraProvider } from '@chakra-ui/react';
@@ -21,7 +21,6 @@ export default function Main() {
         >
       
         </Route>
-        <Route path="rtl/*" element={<RTLLayout theme={currentTheme} setTheme={setCurrentTheme} />} />
         <Route path="/" element={<Navigate to="/admin" replace />} />
       </Routes>
     </ChakraProvider>
